3d50f027e931314e9c1addd908b9dd245c908f2b
seshasai-VVIT539 <seshasai8121@gmail.com>
2022-04-22 14:20:34 +0530

    sharing location

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,3 +10,15 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('geolocation is not supported by your browser')
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
