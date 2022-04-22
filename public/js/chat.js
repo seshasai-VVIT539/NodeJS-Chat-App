2b757ca5d010ba88fef0fc9eabf757d0fe2cd084
seshasai-VVIT539 <seshasai8121@gmail.com>
2022-04-22 10:55:21 +0530

    chat app sending messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,12 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('Count has been updated!', count);
+socket.on('message', (msg) => {
+    console.log('Message received is :=> ', msg);
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('button clicled');
-    socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message)
 })
